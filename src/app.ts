619f29e9aadf87ea1a20bd3492ef16dfbf31d72b
SALAUDEEN, K.R <ridwan.salaudeen@alerzo.com>
2025-10-25 21:22:11 +0100

    Feature (shutdown): handle how server shutdown

--- src/app.ts
+++ src/app.ts
@@ -14,6 +14,29 @@ dotenv.config();
 
 const PORT = process.env.PORT ? Number(process.env.PORT) : 3000;
 
+// Define shutdown function
+function shutdown(signal?: string) {
+    console.log(`Received ${signal ?? 'exit signal'}, shutting down...`);
+    process.exit(0);
+}
+
+// Attach signals to shutdown
+process.on('SIGINT', () => shutdown('SIGINT'));
+process.on('SIGTERM', () => shutdown('SIGTERM'));
+
+// handle unhandled errors shutdown
+process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    shutdown();
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+    console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+    shutdown();
+});
+
+
+// Start Server
 async function start() {
 
     // Initialize Database
@@ -35,7 +58,7 @@ async function start() {
     const droneRouter = registerDroneRoutes();
     app.use('/api', droneRouter);
 
-    app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    const server = app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
 
     // Battery checker: runs every 10 minutes and writes battery log entries  
     setInterval(async () => {
@@ -56,6 +79,18 @@ async function start() {
 
         }
     }, 1000 * 60 * 10);
+
+    // exit (close server before shutting down)
+    function gracefulExit(signal?: string) {
+        console.log(`Received ${signal ?? 'exit'}, closing server...`);
+        server.close(() => {
+            console.log('Server closed.');
+            process.exit(0);
+        });
+    }
+
+    process.on('SIGINT', () => gracefulExit('SIGINT'));
+    process.on('SIGTERM', () => gracefulExit('SIGTERM'));
 
 }
 
